0a25f1f5e06f2d663c08ca776d110be89a724a57
ace-spadez <anvay200.1@gmail.com>
2020-11-14 01:03:35 +0530

    21CARD: made the trick

--- frontend/src/components/hand.component.js
+++ frontend/src/components/hand.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import '../App.css';
+// export default 
 export default class HAND extends Component {
 
 
@@ -14,11 +15,16 @@ export default class HAND extends Component {
 
     render() {
         return (
-            <div>
+            <div style={{position:"static"}}>
                 {this.props.hand.map((e, index) => {
-                    return <div style={{position:"absolute",margin:"50px"}}>
+                    if(this.props.focused==-1 || index+1!=this.props.focused)
+                    return <div style={{position:"absolute",margin:`${this.props.margin}px`}}>
                         <img className="hand-card"  key={e} src={require(`../card_pics/${e}.png`)} width={`${this.props.width}px`} style={{transform:`rotateZ(${(index-this.props.hand.length)*8}deg)`, transformOrigin:'bottom left'}}></img>
-                    </div>
+                    </div>;
+                    else 
+                    return <div style={{position:"absolute",margin:`${this.props.margin}px`}}>
+                    <img className="hand-card-focused"  key={e} src={require(`../card_pics/${e}.png`)} width={`${this.props.width}px`} style={{transform:`rotateZ(${(index-this.props.hand.length)*8}deg)`, transformOrigin:'bottom left'}}></img>
+                </div>;
 
 
                 })}
