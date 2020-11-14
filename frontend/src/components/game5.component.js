9882b958b5726fdb44c48c96e8160046a07ca1ba
ace-spadez <anvay200.1@gmail.com>
2020-11-14 10:05:36 +0530

    Play Page: made needed UI changes

--- frontend/src/components/game5.component.js
+++ frontend/src/components/game5.component.js
@@ -12,107 +12,86 @@ export default class GAME5 extends Component {
             limit:11
         }
     }
-    // componentDidMount() {
+    // // componentDidMount() {
         
-    //     // axios.get('http://localhost:4000/a')
-    //     // .then(response => {
-    //     //     this.setState({cards1: response.data});
-    //     //     axios.get('http://localhost:4000/b')
-    //     //     .then(response => {
-    //     //         this.setState({cards2: response.data});
-    //     //     })
-    //     // })
+    // //     // axios.get('http://localhost:4000/a')
+    // //     // .then(response => {
+    // //     //     this.setState({cards1: response.data});
+    // //     //     axios.get('http://localhost:4000/b')
+    // //     //     .then(response => {
+    // //     //         this.setState({cards2: response.data});
+    // //     //     })
+    // //     // })
+    // // }
+    // Show_logic=() => {
+    //     if(!window.confirm("UNLOCK LOGIC ?"))
+    //         return 
+    //     this.props.history.push({
+    //         pathname:'/Play/Game1/Logic'
+    //     });
     // }
-    Show_logic=() => {
-        if(!window.confirm("UNLOCK LOGIC ?"))
-            return 
-        this.props.history.push({
-            pathname:'/Play/Game1/Logic'
-        });
-    }
-    Shuffle_left=() => {
-       this.setState({total_count:this.state.total_count+1})
-       if(this.state.total_count >=  this.state.limit)
-       {
-            alert("Finished Shuffling,you can see first element of both are " + String(this.state.cards1[0]))
+    // Shuffle_left=() => {
+    //    this.setState({total_count:this.state.total_count+1})
+    //    if(this.state.total_count >=  this.state.limit)
+    //    {
+    //         alert("Finished Shuffling,you can see first element of both are " + String(this.state.cards1[0]))
 
-            this.state.cards1.shift();
-            this.state.cards2.shift();
-            this.setState({total_count:0});
-            if(this.state.cards1.length == 1)
-             {
-                 let check=window.confirm("Game Over \n Play again? ")
-                if(check)
-                 {
-                     this.setState({cards1:[1,2,3,4],cards2:[4,3,2,1],total_count:0,limit:11})
-                 }
-                 else
-                 {
-                     this.props.history.push({pathname:'/'});
-                 }
-             }
-            return; 
-       }
-       else
-       {
-            this.state.cards1.push(this.state.cards1.shift())
-       }
-    }
-    Shuffle_right=() => {
-        this.setState({total_count:this.state.total_count+1})
-        if(this.state.total_count >=  this.state.limit)
-        {
-             alert("Finished Shuffling,you can see first element of both are " + String(this.state.cards1[0]))
-             this.state.cards1.shift();
-             this.state.cards2.shift();
-             this.setState({total_count:0});
-             if(this.state.cards1.length == 1)
-             {
-                 if(window.confirm("Game Over !! \n Play Again ? "))
-                 {
+    //         this.state.cards1.shift();
+    //         this.state.cards2.shift();
+    //         this.setState({total_count:0});
+    //         if(this.state.cards1.length == 1)
+    //          {
+    //              let check=window.confirm("Game Over \n Play again? ")
+    //             if(check)
+    //              {
+    //                  this.setState({cards1:[1,2,3,4],cards2:[4,3,2,1],total_count:0,limit:11})
+    //              }
+    //              else
+    //              {
+    //                  this.props.history.push({pathname:'/'});
+    //              }
+    //          }
+    //         return; 
+    //    }
+    //    else
+    //    {
+    //         this.state.cards1.push(this.state.cards1.shift())
+    //    }
+    // }
+    // Shuffle_right=() => {
+    //     this.setState({total_count:this.state.total_count+1})
+    //     if(this.state.total_count >=  this.state.limit)
+    //     {
+    //          alert("Finished Shuffling,you can see first element of both are " + String(this.state.cards1[0]))
+    //          this.state.cards1.shift();
+    //          this.state.cards2.shift();
+    //          this.setState({total_count:0});
+    //          if(this.state.cards1.length == 1)
+    //          {
+    //              if(window.confirm("Game Over !! \n Play Again ? "))
+    //              {
                      
-                     this.setState({cards1:[1,2,3,4],cards2:[4,3,2,1],total_count:0,limit:11})
-                 }
-                 else
-                 {
-                     this.props.history.push({pathname:'/'});
-                 }
-             }
+    //                  this.setState({cards1:[1,2,3,4],cards2:[4,3,2,1],total_count:0,limit:11})
+    //              }
+    //              else
+    //              {
+    //                  this.props.history.push({pathname:'/'});
+    //              }
+    //          }
             
-             return; 
-        }
-        else
-        {
-            this.state.cards2.push(this.state.cards2.shift())
-        }
-     }
+    //          return; 
+    //     }
+    //     else
+    //     {
+    //         this.state.cards2.push(this.state.cards2.shift())
+    //     }
+    //  }
     render() {
         return (
             <div>
             game5
             <button type="button" onClick={this.Show_logic} style={{float: 'right'}} > Show Logic </button>
-          <div>
-
-              <div>
-                {this.state.cards1}
-              </div>
-              <div>
-                {this.state.cards2}
-              </div>
-              <div >
-                operations_left: {this.state.limit-this.state.total_count}
-              </div>
-          </div>
-          <div className="container">
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-              <li className="navbar-item"><button type="button" onClick={this.Shuffle_left}>Left</button></li>
-              <li className="navbar-item"><button type="button" onClick={this.Shuffle_right}>Right</button></li>
-          </ul>
-      </div>
-  </nav>
-</div>
+    
           </div>
       )
     }
--- frontend/src/components/play.component.js
+++ frontend/src/components/play.component.js
@@ -1,53 +1,99 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import '../App.css';
+import { wrap } from 'module';
 // import * as contentful from 'contentful'
 
 
 export default class PLAY extends Component {
-    
-    
-    Game1=() => {
-        this.props.history.push({
-        pathname:'/Play/Game1/'
-      });
-    }
-    Game2=() => {
-        this.props.history.push({
-        pathname:'/Play/Game2/'
-      });
-    }
-    Game3=() => {
-        this.props.history.push({
-        pathname:'/Play/Game3/'
-      });
-    }
-    Game4=() => {
-        this.props.history.push({
-        pathname:'/Play/Game4/'
-      });
-    }
-    Game5=() => {
-      this.props.history.push({
-      pathname:'/Play/Game5/'
+
+
+  Game1 = () => {
+    this.props.history.push({
+      pathname: '/Play/Game1/'
     });
   }
-    render() {
-        return (  
-            <div>
+  Game2 = () => {
+    this.props.history.push({
+      pathname: '/Play/Game2/'
+    });
+  }
+  Game3 = () => {
+    this.props.history.push({
+      pathname: '/Play/Game3/'
+    });
+  }
+  Game4 = () => {
+    this.props.history.push({
+      pathname: '/Play/Game4/'
+    });
+  }
+  Game5 = () => {
+    this.props.history.push({
+      pathname: '/Play/Game5/'
+    });
+  }
+  render() {
+    return (
+      <div>
 
-                <h2> Here are the list of some awesome card games you'd enjoy !! </h2>
-                <div>
-                <ol className="navbar-nav mr-auto">
-              <li>Game1<button type="button" style={{float: 'right'}} onClick={this.Game1}>Play</button></li>
-              <li >0-1 Game<button type="button" style={{float: 'right'}} onClick={this.Game2}>Play</button></li>
-              <li >Game3<button type="button" style={{float: 'right'}} onClick={this.Game3}>Play</button></li>
-              <li >Game4<button type="button" style={{float: 'right'}} onClick={this.Game4}>Play</button></li>
-              <li >Game5<button type="button" style={{float: 'right'}} onClick={this.Game5}>Play</button></li>
-            </ol>
+        {/* <h2> Here are the list of some awesome card games you'd enjoy !! </h2> */}
+        {/* <div>
+          <ol className="navbar-nav mr-auto">
+            <li>Game1<button type="button" style={{ float: 'right' }} onClick={this.Game1}>Play</button></li>
+            <li >0-1 Game<button type="button" style={{ float: 'right' }} onClick={this.Game2}>Play</button></li>
+            <li >Game3<button type="button" style={{ float: 'right' }} onClick={this.Game3}>Play</button></li>
+            <li >Game4<button type="button" style={{ float: 'right' }} onClick={this.Game4}>Play</button></li>
+            <li >Game5<button type="button" style={{ float: 'right' }} onClick={this.Game5}>Play</button></li>
+          </ol>
+        </div> */}
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          <div className="card play-card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src={require('../assets/27cards.jpg')} alt="27 cards trick" />
+            <div className="card-body">
+              <h5 className="card-title">27 Cards</h5>
+              <p className="card-text">Choose the decks your card is in, while we guess your card!</p>
+              <button className="btn btn-primary" onClick={this.Game4}>Play</button>
             </div>
-        
+          </div>
+          <div className="card play-card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src={require('../assets/chinese.jpg')} alt="Chinese Remainder" />
+            <div className="card-body">
+              <h5 className="card-title">Chinese Remainder</h5>
+              <p className="card-text">Rotate either of the decks fixed number of times and let the magic unfold!!</p>
+              <button className="btn btn-primary" onClick={this.Game1}>Play</button>
             </div>
+          </div>
+          <div className="card play-card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src={require('../assets/king-queen.jpg')} alt="Zero-One" />
+            <div className="card-body">
+              <h5 className="card-title">King-Queen</h5>
+              <p className="card-text">Remove king-queen pairs one by one to see who wins!!! </p>
+              <button className="btn btn-primary" onClick={this.Game2}>Play</button>
+            </div>
+          </div>
+          <div className="card play-card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src={require('../assets/hamiltonian.jpg')} alt="Hamiltonian" />
+            <div className="card-body">
+              <h5 className="card-title">Square Sum</h5>
+              <p className="card-text">Arrange the numbers such that any adjacent numbers' sum is a perfect square!! </p>
+              <button className="btn btn-primary" onClick={this.Game3}>Play</button>
+            </div>
+          </div>
+          <div className="card play-card" style={{ width: "18rem" }}>
+            <img className="card-img-top" src={require('../assets/kruskal.jpg')} alt="Kruskal" />
+            <div className="card-body">
+              <h5 className="card-title">Kruskal</h5>
+              <p className="card-text">!!! SPOILER ALERT !!! </p>
+              <p className="card-text">You will be mind blown!!! </p>
+              <button className="btn btn-primary" onClick={this.Game5}>Play</button>
+            </div>
+          </div>
+        </div>
 
-        )
-    }
+
+      </div>
+
+    )
+  }
 }
